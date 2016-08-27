cc0c9c9a40f04e9afd8297b17f4f8ca2b067b9da
Bruno Novaes Camera Santos <bcamera@users.noreply.github.com>
2016-08-27 17:51:40 -0300

    Atualização

--- Linux/atualizar_e_limpar.sh
+++ Linux/atualizar_e_limpar.sh
@@ -7,10 +7,12 @@ echo "Instalando Programas"
 echo ""
 #Para instalar virtual box remova o comentario da linha abaixo
 #apt-get install virtualbox -y
+apt-get install terminator -y
+apt-get install openvpn -y
 apt-get install rar -y
 apt-get install gnupg2 -y
 apt-get install git -y
-apt-get install g++ -y
+#apt-get install g++ -y
 apt-get install default-jdk -y
 git config --global user.name "bcamera"
 git config --global user.email bcamera@hotmail.com
