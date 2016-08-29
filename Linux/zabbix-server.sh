a1d56b89afc53f4ed023b3fd0287c54dca542bfe
Bruno Novaes Camera Santos <bcamera@users.noreply.github.com>
2016-08-29 19:06:08 -0300

    Correção do codigo

--- Linux/zabbix-server.sh
+++ Linux/zabbix-server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-su
-cp -Ri /Documents/Zabbix-Server/etc /etc
-cp -Ri /Documents/Zabbix-Server/usr/ /usr
+tar -xvzf Zabbix.tar.gz 
+cd Zabbxix-Server/etc
+sudo cp -Ri mysql/ /etc/
+sudo cp -Ri php5/ /etc/
+sudo cp -Ri apache2/sites-available/zabbix.conf /etc/apache2/sites-available/zabbix.conf
+sudo cp -Ri zabbix/ /etc/
+cd ..
+cd usr
+sudo cp -Ri mysql/ /usr/
+sudo cp -Ri php5/ /usr/
+sudo cp -Ri zabbix/ /usr/
